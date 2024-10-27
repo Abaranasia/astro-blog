739717bfc9fbb8a063d061681258257e78d78cce
Abaranasia <rankirlian@gmail.com>
2024-10-27 19:20:39 +0100

    Update: replace image string type by image to refine it

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection= defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: z.string(),
-        
+        //image: z.string(),
+        image: image().refine( (img) => img.width > 500, {
+            message: 'Image should be greater than 500px',
+        }),
         // Relation
         author: z.string(),
 
